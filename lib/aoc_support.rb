18811b8c1f673ffbb79f47aa043348b816a33547
Arca <arca.artem@gmail.com>
2024-12-04 23:12:15 +0000

    Day 3 solutions

--- lib/aoc_support.rb
+++ lib/aoc_support.rb
@@ -7,19 +7,19 @@ class AoCSolution
 
   def input_lines
     Enumerator.new do |yielder|
-      File.open(input_file, 'r') do |f|
+      File.open(input_file_path, 'r') do |f|
         f.each_line { |line| yielder << line }
       end
     end
   end
 
   def input_text
-    File.read(input_file)
+    File.read(input_file_path)
   end
 
   private
 
-  def input_file
+  def input_file_path
     "#{File.dirname(caller_locations[1].path)}/input.txt"
   end
 end
