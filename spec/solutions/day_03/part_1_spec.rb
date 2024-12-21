471b630bf69cf60f1558e5c6b82f3bc89e9643d1
Arca <arca.artem@gmail.com>
2024-12-21 19:36:41 +0000

    Day 3 solutions

---
+++ spec/solutions/day_03/part_1_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Day3Part1 do
+  it_behaves_like 'a solution', for_input: 'input.txt', and_returns: 187_825_547
+end
---
+++ spec/solutions/day_03/part_2_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Day3Part2 do
+  it_behaves_like 'a solution', for_input: 'input.txt', and_returns: 85_508_223
+end
